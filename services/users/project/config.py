3333c72e6757e25cc8fabb062e81efe008fa5d91
FormusohDinga <rformusoh1@yahoo.com>
2018-12-10 02:50:31 -0600

    Initial Commit after getting react-service docker-ed

---
+++ services/users/project/config.py
@@ -0,0 +1,21 @@
+# services/users/project/config.py
+import os
+
+class BaseConfig:
+    # Base Configuration
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "my_precious"
+
+
+class DevelopmentConfig(BaseConfig):
+    #Dev configuration
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestingConfig(BaseConfig):
+    #Testing configuration
+    TESTING=True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
